9b5cfaa65281da6c15a8c7f8dc4bccb015a0e9a7
DAQEM <daqemyt@gmail.com>
2021-12-09 19:17:49 +0100

    Disabled Items and Blocks for Jobs+ 0.1.1 beta release.

--- src/main/java/me/daqem/jobsplus/JobsPlus.java
+++ src/main/java/me/daqem/jobsplus/JobsPlus.java
@@ -1,25 +1,21 @@
 package me.daqem.jobsplus;
 
-import me.daqem.jobsplus.init.ModItems;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.NotNull;
 
 @Mod(JobsPlus.MOD_ID)
 public class JobsPlus {
 
     public static final String MOD_ID = "jobsplus";
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final CreativeModeTab TAB = new CreativeModeTab("jobsplustab") {
-        public @NotNull ItemStack makeIcon() {
-            return new ItemStack(ModItems.LEVEL_100_TROPHY.get());
-        }
-    };
+//    public static final CreativeModeTab TAB = new CreativeModeTab("jobsplustab") {
+//        public @NotNull ItemStack makeIcon() {
+//            return new ItemStack(ModItems.LEVEL_100_TROPHY.get());
+//        }
+//    };
 
     public JobsPlus() {
         DistExecutor.safeRunForDist(
--- src/main/java/me/daqem/jobsplus/common/block/BlockItemBase.java
+++ src/main/java/me/daqem/jobsplus/common/block/BlockItemBase.java
@@ -1,14 +1,14 @@
 package me.daqem.jobsplus.common.block;
 
-import me.daqem.jobsplus.JobsPlus;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
 
 public class BlockItemBase extends BlockItem {
 
 
     public BlockItemBase(Block p_40565_) {
-        super(p_40565_, new Item.Properties().tab(JobsPlus.TAB));
+        super(p_40565_, new Properties().tab(CreativeModeTab.TAB_FOOD)); //TODO Temporary because of error.
+//        super(p_40565_, new Item.Properties().tab(JobsPlus.TAB));
     }
 }
