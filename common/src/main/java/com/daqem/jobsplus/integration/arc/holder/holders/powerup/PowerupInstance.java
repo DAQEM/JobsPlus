1592d5f5b7dd69c2177dd3b4c27926092eaf63b4
DAQEM <daqemyt@gmail.com>
2025-03-15 12:05:29 +0100

    Fixes NullReferenceException

--- common/src/main/java/com/daqem/jobsplus/integration/arc/holder/holders/powerup/PowerupInstance.java
+++ common/src/main/java/com/daqem/jobsplus/integration/arc/holder/holders/powerup/PowerupInstance.java
@@ -88,7 +88,8 @@ public class PowerupInstance extends AbstractActionHolder {
     public boolean passedHolderCondition(ActionData actionData) {
         ArcPlayer arcPlayer = actionData.getPlayer();
         if (arcPlayer instanceof JobsPlayer jobsPlayer) {
-            Job job = jobsPlayer.jobsplus$getJobs().stream().filter(job1 -> job1.getJobInstance().getLocation().equals(this.getJobLocation())).findFirst().orElse(null);
+            Job job = jobsPlayer.jobsplus$getJobs().stream()
+                    .filter(job1 -> job1 != null && job1.getJobInstance() != null && job1.getJobInstance().getLocation().equals(this.getJobLocation())).findFirst().orElse(null);
             if (job != null) {
                 Powerup powerup = job.getPowerupManager().getAllPowerups().stream().filter(powerup1 -> powerup1.getPowerupInstance().getLocation().equals(this.getLocation())).findFirst().orElse(null);
                 if (powerup != null) {
